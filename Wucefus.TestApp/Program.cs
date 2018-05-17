e42ababebc1737e1ecac2d16272db36b5b5fbd10
t4rn <kwitkowski.it@gmail.com>
2018-05-17 11:54:09 +0200

    Console Test App

---
+++ Wucefus.TestApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wucefus.TestApp.SoapConsents;
+
+namespace Wucefus.TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetConsents();
+
+            Console.Read();
+        }
+
+        private static void GetConsents()
+        {
+            using (ConsentsClient client = new ConsentsClient())
+            {
+                // not necessary
+                client.ClientCredentials.UserName.UserName = "user";
+                client.ClientCredentials.UserName.Password = "p@ssw0rd";
+                var result = client.GetConsentsAll();
+
+                string jsonResult = Serialize(result);
+
+                Console.WriteLine($"result:\n{jsonResult}");
+            }
+        }
+
+        private static string Serialize<T>(T result)
+        {
+            return JsonConvert.SerializeObject(result,
+                    new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore });
+        }
+    }
+}
